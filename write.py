459fd68a5fe657fb2a3687709c8bbfbd4fa6c743
Mircea N <nunu.mircea@gmail.com>
2023-05-14 08:12:30 -0400

    write.py update1
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- write.py
+++ write.py
@@ -15,18 +15,20 @@ from datetime import date
 def write_instructors():
     # Add instructors
     # Create a user
-    user_john = User(first_name='John', last_name='Doe', dob=date(1962, 7, 16))
+    #user_john = User(first_name='John', last_name='Doe', dob=date(1962, 7, 16))
+    user_john= User.objects.create_user(username='John_Doe', first_name='John', last_name='Doe',
+                                            password='pass12345')
     user_john.save()
     instructor_john = Instructor(full_time=True, total_learners=30050)
     # Update the user reference of instructor_john to be user_john
     instructor_john.user = user_john
     instructor_john.save()
     
-    instructor_yan = Instructor(first_name='Yan', last_name='Luo', dob=date(1962, 7, 16), full_time=True, total_learners=30050)
-    instructor_yan.save()
+    # instructor_yan = Instructor(first_name='Yan', last_name='Luo', dob=date(1962, 7, 16), full_time=True, total_learners=30050)
+    # instructor_yan.save()
 
-    instructor_joy = Instructor(first_name='Joy', last_name='Li', dob=date(1992, 1, 2), full_time=False, total_learners=10040)
-    instructor_joy.save()
-    instructor_peter = Instructor(first_name='Peter', last_name='Chen', dob=date(1982, 5, 2), full_time=True, total_learners=2002)
-    instructor_peter.save()
+    # instructor_joy = Instructor(first_name='Joy', last_name='Li', dob=date(1992, 1, 2), full_time=False, total_learners=10040)
+    # instructor_joy.save()
+    # instructor_peter = Instructor(first_name='Peter', last_name='Chen', dob=date(1982, 5, 2), full_time=True, total_learners=2002)
+    # instructor_peter.save()
     print("Instructor objects all saved... ")
