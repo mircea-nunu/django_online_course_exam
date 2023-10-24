97dcdf7361ea5c46fbce3e2694c3861349cf9220
mircea-nunu <nunu.mircea@gmail.com>
2023-10-24 20:56:16 +0300

    update 24.10.2023

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ python3 manage.py makemigrations onlinecourse
 python3 manage.py sqlmigrate onlinecourse 0001
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+python3 write.py
 python3 manage.py runserver 0.0.0.0:8000
